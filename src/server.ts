2424a00f0d3c418a096e8d7f29ffad0cbc150b61
Bruno Souza <bruno1994deilson@gmail.com>
2021-04-20 15:53:26 -0300

    Criando repositorio e routes

--- src/server.ts
+++ src/server.ts
@@ -2,29 +2,10 @@ import express from 'express';
 
 import "./database";
 
+import { routes } from "./routes";
+
 const app = express();
 
-/**
- * GET = Buscas
- * POST = Criação
- * PUT = Alteração
- * DELETE = Deletar
- * PATCH = Alterar uma informação especifica 
- */
-
-// app.get("/", (request, response) => {
-//     return response.send("Ola NLW 05");
-// });
-
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Ola NLW 05!",
-    })
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Usuário salvo com sucesso!" });
-});
-
+app.use(routes);
 
 app.listen(3333, () => console.log("Servers running on port 3333"));
