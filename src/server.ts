e1fb2533ad9131e3eb2ecd15cb2f27862178095d
Bruno Souza <bruno1994deilson@gmail.com>
2021-04-20 13:24:47 -0300

    Primeira rota criada

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+
+const app = express();
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar uma informação especifica 
+ */
+
+// app.get("/", (request, response) => {
+//     return response.send("Ola NLW 05");
+// });
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Ola NLW 05!",
+    })
+});
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Usuário salvo com sucesso!" });
+});
+
+
+app.listen(3333, () => console.log("Servers running on port 3333"));
